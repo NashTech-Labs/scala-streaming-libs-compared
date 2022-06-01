f3eaeed3761b4ab7cce074c66231af500fcfe516
Sebastian Harko <sebastian.harko@protonmail.com>
2022-06-01 10:09:43 +0200

    test

---
+++ akka-streams/src/main/scala/com/example/InfiniteStreamQuickstart.scala
@@ -0,0 +1,24 @@
+package com.example
+
+import akka.stream._
+import akka.stream.scaladsl._
+import akka.{Done, NotUsed}
+import akka.actor.ActorSystem
+import akka.util.ByteString
+import scala.concurrent._
+import scala.concurrent.duration._
+import java.nio.file.Paths
+import akka.event.Logging
+
+object InfiniteStreamQuickstart extends App {
+
+  implicit val system: ActorSystem = ActorSystem("QuickStart")
+  val logger = Logging(system, "QuickStart")
+
+  Source(1 to 100)
+    .map(item => item + 42)
+    .wireTap(item => logger.info(s"Got ${item}!"))
+    .to(Sink.ignore)
+    .run()
+
+}
