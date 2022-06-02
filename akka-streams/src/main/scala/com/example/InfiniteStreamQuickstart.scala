044ee8792c1702e60cf90444d0f9f248208137eb
Sebastian Harko <sebastian.harko@protonmail.com>
2022-06-02 09:47:12 +0200

    infinite stream with source cycle

--- akka-streams/src/main/scala/com/example/InfiniteStreamQuickstart.scala
+++ akka-streams/src/main/scala/com/example/InfiniteStreamQuickstart.scala
@@ -9,14 +9,15 @@ import scala.concurrent._
 import scala.concurrent.duration._
 import java.nio.file.Paths
 import akka.event.Logging
+import scala.util.Random
 
 object InfiniteStreamQuickstart extends App {
 
   implicit val system: ActorSystem = ActorSystem("QuickStart")
   val logger = Logging(system, "QuickStart")
 
-  Source(1 to 100)
-    .map(item => item + 42)
+  Source
+    .cycle(() => Iterator.single(Random.nextInt(10)))
     .wireTap(item => logger.info(s"Got ${item}!"))
     .to(Sink.ignore)
     .run()
